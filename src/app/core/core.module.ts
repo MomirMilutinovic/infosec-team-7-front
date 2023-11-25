bcd447fa4f663fc2666b2895c132b837c639456e
Momir Milutinović <milutinm462@gmail.com>
2023-11-25 15:32:33 +0100

    fix: Remove unneeded imports

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,23 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { HeaderComponent } from './header/header.component';
-import { ContainerComponent } from './container/container.component';
-
 
 @NgModule({
   declarations: [
-    NavBarComponent,
-    HeaderComponent,
-    ContainerComponent,
+    NavBarComponent
   ],
   imports: [
     CommonModule,
   ],
   exports: [
-    NavBarComponent,
-    HeaderComponent,
-    ContainerComponent,
+    NavBarComponent
   ]
 })
 export class CoreModule { }
