fd10d6e489f4d347cacf3857fd0d885d86a741dd
dusanlecic <dusan.lecic5@gmail.com>
2023-11-26 23:55:11 +0100

    add: page component, might be unnecessary

---
+++ src/app/core/page/page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-page',
+  templateUrl: './page.component.html',
+  styleUrl: './page.component.css'
+})
+export class PageComponent {
+
+}
