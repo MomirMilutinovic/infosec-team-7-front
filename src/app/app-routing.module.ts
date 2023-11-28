e6ed59034f44ce343fd0aebca76d4d1195546d04
Momir Milutinović <milutinm462@gmail.com>
2023-11-28 20:03:27 +0100

    feat: Add route to profile screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
+import { ProfileComponent } from './profile/profile/profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'properties', pathMatch: 'full' },
   { path: 'properties', loadChildren: () => import('./properties/properties.module').then(m => m.PropertiesModule) },
+  { path: 'profile', component: ProfileComponent}
 ];
 
 @NgModule({
