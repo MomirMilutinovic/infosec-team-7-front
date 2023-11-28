8877632b86edbeedb6e7199d7537f7a6cd5d1555
Momir Milutinović <milutinm462@gmail.com>
2023-11-28 21:46:40 +0100

    fix: Fix form field display in profile component

--- src/app/profile/info-form/info-form.component.ts
+++ src/app/profile/info-form/info-form.component.ts
@@ -4,6 +4,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule} from '@angular/material/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -19,5 +20,14 @@ import {MatNativeDateModule} from '@angular/material/core';
   ],
 })
 export class InfoFormComponent {
+
+  infoForm = new FormGroup({ 
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required]),
+    address: new FormControl('', [Validators.required]),
+    country: new FormControl('', [Validators.required])
+  });
 
 }
