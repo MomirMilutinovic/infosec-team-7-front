b50c7b0efd338e2a1da0ae1951774b9fb51bbb82
dusanlecic <dusan.lecic5@gmail.com>
2023-11-28 16:33:02 +0100

    update: new routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
+import { LandingComponent } from './landing/landing/landing.component';
+import { ProfileComponent } from './profile/profile/profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'properties', pathMatch: 'full' },
+  { path: 'home', component: LandingComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: 'properties', loadChildren: () => import('./properties/properties.module').then(m => m.PropertiesModule) },
 ];
 
