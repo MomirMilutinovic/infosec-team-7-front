94c1b65e6850196a7b37f31c7a96835933b76d34
dusanlecic <dusan.lecic5@gmail.com>
2023-11-28 16:26:48 +0100

    update: routing and new component

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -6,12 +6,17 @@ import { BasicInfoComponent } from './basic-info/basic-info.component';
 import { InfoFormComponent } from './info-form/info-form.component';
 import { ManagementButtonsComponent } from './management-buttons/management-buttons.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: '', component: ProfileComponent },
+];
 
 @NgModule({
   declarations: [
@@ -20,6 +25,8 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
     InfoFormComponent,
     ManagementButtonsComponent,
     EditProfileComponent,
+    ChangePasswordComponent,
+
   ],
   imports: [
     CommonModule,
@@ -29,6 +36,7 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
     MatFormFieldModule,
     MatNativeDateModule,
     MatDatepickerModule,
+    RouterModule.forChild(routes)
   ],
   exports: [
     ProfileComponent,
@@ -36,6 +44,8 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
     InfoFormComponent,
     ManagementButtonsComponent,
     EditProfileComponent,
+    ChangePasswordComponent,
+    RouterModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
