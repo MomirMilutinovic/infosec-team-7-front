2803ae1fd37e7554c8a6e68fdc139b45cb559994
Momir Milutinović <milutinm462@gmail.com>
2023-11-29 13:22:01 +0100

    refactor: Remove unused PageComponent

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
 import { ContainerComponent } from './container/container.component';
-import { PageComponent } from './page/page.component';
 import { HelperMenuComponent } from './helper-menu/helper-menu.component';
 import { AccountModule } from '../account/account.module';
 import { MaterialModule } from '../../infrastructure/material/material.module';
@@ -12,7 +11,6 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 @NgModule({
   declarations: [
     HelperMenuComponent,
-    PageComponent,
     NavBarComponent,
     HeaderComponent,
     ContainerComponent,
@@ -26,7 +24,6 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   ],
   exports: [
     HelperMenuComponent,
-    PageComponent,
     NavBarComponent,
     HeaderComponent,
     ContainerComponent,
